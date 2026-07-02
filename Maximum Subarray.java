afd3496a3d63fc8c3b55b39cd020cfaf1348abf6
thushita04-bit <thushita04@gmail.com>
2026-07-02 08:38:33 +0530

    Maximum Subarray.java

---
+++ Maximum Subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+     int maxsum=nums[0];
+     int currentsum=0;
+     for(int i=0;i<nums.length;i++){
+        currentsum+=nums[i];
+        if(currentsum>maxsum){
+            maxsum=currentsum;
+        }
+        if(currentsum<0){
+            currentsum=0;
+
+        }
+     }
+     return maxsum;
+
+    }
+}
